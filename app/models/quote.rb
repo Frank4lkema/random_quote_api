d545ba6a12ac6408f853abecefa0887d938b9444
Frank <frank@jaamo.nl>
2021-09-28 21:28:30 +0200

    Added the validations and associations of the created models

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,2 +1,7 @@
 class Quote < ApplicationRecord
+  # model association
+  has_many :quote_ratings, dependent: :destroy
+
+  #validations
+  validates_presence_of :quote, :author
 end
--- app/models/quote_rating.rb
+++ app/models/quote_rating.rb
@@ -1,3 +1,7 @@
 class QuoteRating < ApplicationRecord
+  # model association
   belongs_to :quote
+
+  # validations
+  validates_presence_of :liked
 end
