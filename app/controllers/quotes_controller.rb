db20d4c10feb9591fef83877e06033326f92d7c5
Frank <frank@jaamo.nl>
2021-09-28 22:10:46 +0200

    Made the last tests pass

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,10 +1,12 @@
 class QuotesController < ApplicationController
 
   def index
-
+    @quotes = Quote.all
+    json_response(@quotes)
   end
 
   def show
-
+    @quote = Quote.find(params[:id])
+    json_response(@quote)
   end
 end
