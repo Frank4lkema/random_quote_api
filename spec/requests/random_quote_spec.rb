d49c169d4973a2b44d5aa85f26fa2586928af2fa
Frank <frank@jaamo.nl>
2021-09-29 19:58:51 +0200

    Made a new request test for the random quote

---
+++ spec/requests/random_quote_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe 'Random Quote endpoint', type: :request do
+  let!(:quotes) { create_list(:quote, 10) }
+
+  # Test suite for GET /todos
+  describe 'GET /quotes' do
+    before { get '/random-quote' }
+
+    it 'returns quotes' do
+      expect(json).not_to be_empty
+      expect(json.size).to eq(1)
+    end
+
+    it 'returns status code 200' do
+      expect(response).to have_http_status(200)
+    end
+  end
+end
