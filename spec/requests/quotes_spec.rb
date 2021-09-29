fb6b52ac4d6b06373e2cc7c19295ebd92dd0a3c0
Frank <frank@jaamo.nl>
2021-09-29 21:52:15 +0200

    Fixed Rubocop warnings

--- spec/requests/quotes_spec.rb
+++ spec/requests/quotes_spec.rb
@@ -1,47 +1,47 @@
 # frozen_string_literal: true
 
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.describe 'Quotes endpoint', type: :request do
+RSpec.describe "Quotes endpoint", type: :request do
   let!(:quotes) { create_list(:quote, 10) }
   let(:quote_id) { quotes.first.id }
 
   # Test suite for GET /todos
-  describe 'GET /quotes' do
-    before { get '/quotes' }
+  describe "GET /quotes" do
+    before { get "/quotes" }
 
-    it 'returns quotes' do
+    it "returns quotes" do
       expect(json).not_to be_empty
       expect(json.size).to eq(10)
     end
 
-    it 'returns status code 200' do
+    it "returns status code 200" do
       expect(response).to have_http_status(200)
     end
   end
 
-  describe 'GET /quotes/:id' do
+  describe "GET /quotes/:id" do
     before { get "/quotes/#{quote_id}" }
 
-    context 'when the record exists' do
-      it 'returns the todo' do
+    context "when the record exists" do
+      it "returns the todo" do
         expect(json).not_to be_empty
-        expect(json['id']).to eq(quote_id)
+        expect(json["id"]).to eq(quote_id)
       end
 
-      it 'returns status code 200' do
+      it "returns status code 200" do
         expect(response).to have_http_status(200)
       end
     end
 
-    context 'when the record does not exist' do
+    context "when the record does not exist" do
       let(:quote_id) { 100 }
 
-      it 'returns status code 404' do
+      it "returns status code 404" do
         expect(response).to have_http_status(404)
       end
 
-      it 'returns a not found message' do
+      it "returns a not found message" do
         expect(response.body).to match(/Couldn't find Quote/)
       end
     end
