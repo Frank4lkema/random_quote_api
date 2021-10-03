82829b0367ce194f68b4556b476e425932c7c33c
Frank <frank@jaamo.nl>
2021-10-03 15:42:22 +0200

    Created the get querry for quote

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -4,5 +4,6 @@ module Types
     # They will be entry points for queries on your schema.
 
     field :get_quotes, resolver: Queries::GetQuotes
+    field :get_quote, resolver: Queries::GetQuote
   end
 end
