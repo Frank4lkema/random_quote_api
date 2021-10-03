aefd4471735adbd1128260480aeab47f345917cb
Frank <frank@jaamo.nl>
2021-10-03 20:13:47 +0200

    Rubocop tweaking

--- app/lib/queries/get_random_quote_based_on_rating.rb
+++ app/lib/queries/get_random_quote_based_on_rating.rb
@@ -9,7 +9,7 @@ module Queries
     private
 
     def quote_ids_with_hig_average
-      QuoteRating.select('quote_id').group('quote_id').average('rating').map { |x| x.first if x.second > 3 }.compact
+      QuoteRating.select("quote_id").group("quote_id").average("rating").map { |x| x.first if x.second > 3 }.compact
     end
   end
 end
