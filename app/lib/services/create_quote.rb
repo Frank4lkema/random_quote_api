6151974b97e857dfa1e84d3b9ba1b343f02e732f
Frank <frank@jaamo.nl>
2021-10-24 15:01:12 +0200

    Made the create quote test pass

--- app/lib/services/create_quote.rb
+++ app/lib/services/create_quote.rb
@@ -2,12 +2,12 @@ module Services
   class CreateQuote
     include Callable
 
-    def initialize(quote_object)
+    def initialize(quote_object:)
       @quote_object = quote_object
     end
 
     def call
-
+      Quote.create(quote: @quote_object.quote, author: @quote_object.author )
     end
   end
 end
--- spec/services/create_quote_spec.rb
+++ spec/services/create_quote_spec.rb
@@ -5,12 +5,12 @@ require "rails_helper"
 module Services
   RSpec.describe CreateQuote do
     subject(:service) { ->(quote_object:) { described_class.call(quote_object: quote_object) } }
-    let(:quote_object) { Struct.new(:quote, :author).new("This is a test quote", "Frank") }
+    let(:quote_object) { Struct.new(:quote, :author) }
 
     describe "#call" do
       context "When services is called" do
         it "expect to create a Quote" do
-          expect { service.call(quote_object: quote_object) }.to change(Quote, :count).by(1)
+          expect { service.call(quote_object: quote_object.new("This is a test quote", "Frank")) }.to change(Quote, :count).by(1)
         end
       end
     end
